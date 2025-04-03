0ff8fb02242233dc1da46acb9fc7903ca3c567cd
priyabahrathi <priyabharathi1003@gmail.com>
2025-04-03 16:52:59 +0530

    Image displayed using Funtional Component

--- src/Store/Slice/ProductSlice.ts
+++ src/Store/Slice/ProductSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Shoe from "../../assets/shoe.png";
+
+const ProductImg = (imageName: string) => `../../src/assets/${imageName}`;
 
 interface Product {
   id: number;
@@ -15,12 +16,12 @@ interface DataState {
 
 const initialState: DataState = {
     Products: [
-        { id: 1, name: 'Black Tee', price: 29.99, category: "Men's", img: Shoe },
-        { id: 2, name: 'Watch', price: 199.99, category: 'Accessories', img: Shoe },
-        { id: 3, name: 'Blue Tee', price: 19.99, category: "Women's", img: Shoe },
-        { id: 4, name: 'Bag', price: 49.99, category: "Women's", img: Shoe },
-        { id: 5, name: 'Shoes', price: 89.99, category: "Men's", img: Shoe },
-        { id: 6, name: 'Watch', price: 99.99, category: 'Accessories', img: Shoe }
+        { id: 1, name: 'Black Tee', price: 29.99, category: "Men's", img: ProductImg("Shoe.png") },
+        { id: 2, name: 'Watch', price: 199.99, category: 'Accessories', img: ProductImg("Shoe.png") },
+        { id: 3, name: 'Blue Tee', price: 19.99, category: "Women's", img: ProductImg("Shoe.png") },
+        { id: 4, name: 'Bag', price: 49.99, category: "Women's", img: ProductImg("Shoe.png") },
+        { id: 5, name: 'Shoes', price: 89.99, category: "Men's", img: ProductImg("Shoe.png") },
+        { id: 6, name: 'Watch', price: 99.99, category: 'Accessories', img: ProductImg("Shoe.png") }
     ],
 };
 
